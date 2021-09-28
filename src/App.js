6c5bb01c5b58c1b2461d74e1cd40c3b880cfbc18
rasimthegrey <rasim.mutlu@protonmail.com>
2021-09-28 12:48:56 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Fragment>
+      <Header />
+      
+      <main>
+        <Meals />
+      </main>
+    </Fragment >
   );
 }
 
